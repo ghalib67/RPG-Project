be229b1ea2d6424596eaf3ee566986ee61cd1f26
ghalib67 <ghalibkaka1234@gmail.com>
2025-12-22 00:47:22 +0500

    created a Player class

--- game.py
+++ game.py
@@ -1,1 +1,20 @@
-print("Hello world!")
+class Player:
+    
+    def __init__(self):
+        #Initializing the values "Player" class will have during the start
+        self.max_hp = 30
+        self.current_hp = 30
+        self.attack_power = 7
+    
+    def decrease_hp(self, damage: int):#Function for Decreasing hp
+        self.current_hp -= damage
+        if self.current_hp > 1:
+            print("GG")
+
+    def incrase_hp(self, healing: int):#Function for Increasing hp
+        self.current_hp += healing
+        if self.current_hp > self.max_hp:
+            self.current_hp = self.max_hp
+
+
+        
